15294028211db2202d1863bb8ba71a0bf8ee84b2
Raphi_55 <35998957+Raphi-Sch@users.noreply.github.com>
2025-11-07 13:18:15 +0100

    Cleanup

--- www/src/js/app.js
+++ www/src/js/app.js
@@ -1,8 +1,3 @@
 import ReVoiceChatAdmin from './app/revoicechatadmin.js';
 
 const RVCA = new ReVoiceChatAdmin();
-
-// Ready state
-document.addEventListener('DOMContentLoaded', function () {
-    document.documentElement.dataset.theme = localStorage.getItem("Theme") || "dark";
-});
